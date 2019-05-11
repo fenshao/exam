0fca11f9e05d4886380314fa12460b5679b02d43
Server <2501734853@qq.com>
2019-05-12 00:20:15 +0800

    添加lib文件

--- out/artifacts/exam_war_exploded/index.jsp
+++ out/artifacts/exam_war_exploded/index.jsp
@@ -12,6 +12,5 @@
     </head>
     <body>
     $END$
-
     </body>
 </html>
--- src/cn/ynni/exam/controllar/Base.java
+++ src/cn/ynni/exam/controllar/Base.java
@@ -2,7 +2,6 @@ package cn.ynni.exam.controllar;
 
 import cn.ynni.exam.utils.MysqlConnection;
 
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
