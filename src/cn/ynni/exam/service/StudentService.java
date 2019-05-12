7c557fc95cc8bae8f8714414c1cc75b5fc31d6f6
lileixer <1035738029@qq.com>
2019-05-12 08:55:46 +0800

    lilei修改的服务层学生

--- src/cn/ynni/exam/service/StudentService.java
+++ src/cn/ynni/exam/service/StudentService.java
@@ -1,5 +1,167 @@
 package cn.ynni.exam.service;
 
+
+import cn.ynni.exam.model.Student;
+import cn.ynni.exam.utils.MysqlConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * author：lilei
+ * fun: 有关学生账号的连接数据库操作，增删改查
+ */
+
 public class StudentService {
+    /**
+     * fun: 插入一个学生
+     * @param stuId
+     * @param stuPassword
+     * @param stuName
+     * @param stuSex
+     * @return void
+     */
+    public void insertStudent(String stuId, String stuPassword, String stuName, String stuSex) {
+        // 取得数据库连接
+        MysqlConnection mysqlConnection = MysqlConnection.getMysqlConnection();
+        Connection conn = mysqlConnection.getCon();
+        String sql = "insert into student (stu_id, stu_password, stu_name, stu_sex) values(?, ?, ?, ?)";
+
+        PreparedStatement stm = null;
+        try {
+
+            stm = conn.prepareStatement(sql);
+            stm.setString(1, stuId);
+            stm.setString(2, stuPassword);
+            stm.setString(3, stuName);
+            stm.setString(4, stuSex);
+            stm.executeUpdate();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * fun: 删除一个学生
+     * @param stuId
+     * @return void
+     */
+    public void deleteStudent(String stuId) {
+        // TODO Auto-generated method stub
+        MysqlConnection mysqlConnection = MysqlConnection.getMysqlConnection();
+        Connection conn = mysqlConnection.getCon();
+        String sql = "delete from student where stu_id = ?";
+        PreparedStatement stm = null;
+        try {
+            stm = conn.prepareStatement(sql);
+            stm.setString(1,stuId);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * fun: 修改一个学生的信息
+     * @param stuId
+     * @param stuPassword
+     * @param stuName
+     * @param stuSex
+     * @return void
+     */
+    public void updateStudent(String stuId, String stuPassword, String stuName, String stuSex){
+        MysqlConnection mysqlConnection = MysqlConnection.getMysqlConnection();
+        Connection conn = mysqlConnection.getCon();
+        String sql = "updata student set stu_password = ?, stu_name = ?, stu_sex = ? where stu_id = ?";
+        PreparedStatement stm = null;
+
+        try {
+            stm = conn.prepareStatement(sql);
+            stm.setString(4,stuId);
+            stm.setString(1,stuPassword);
+            stm.setString(2,stuName);
+            stm.setString(3,stuSex);
+            stm.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * fun: 通过stuId查询一个学生的所有信息
+     * @param stuId
+     * @return Student
+     */
+    public Student queryStudent(String stuId){
+        Student student = new Student();
+        MysqlConnection mysqlConnection = MysqlConnection.getMysqlConnection();
+        Connection conn = mysqlConnection.getCon();
+        String sql = "select * from student where stu_id="+stuId;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            stm = conn.prepareStatement(sql);
+            rs = stm.executeQuery();
+            if(rs.next()){
+                student.setStuId(rs.getString("stu_Id"));
+                student.setStuName(rs.getString("stu_name"));
+                student.setStuPassword(rs.getString("stu_password"));
+                student.setStuSex(rs.getString("stu_sex"));
+
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            mysqlConnection.closeResuletSet(rs);
+        }
+        return student;
+    }
+
+    /**
+     * fun: 查询所有读者的所有信息
+     * @return ArrayList<Student>
+     */
+    public ArrayList<Student> queryAllStudent(){
+        ArrayList<Student> students_Array = new ArrayList<Student>();
+        MysqlConnection mysqlConnection = MysqlConnection.getMysqlConnection();
+        Connection conn = mysqlConnection.getCon();
+        String sql = "select * from admin where status=1";
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            stm = conn.prepareStatement(sql);
+            rs = stm.executeQuery();
+            while(rs.next()){
+                Student student = new Student();
+                student.setStuId(rs.getString("stu_Id"));
+                student.setStuName(rs.getString("stu_name"));
+                student.setStuPassword(rs.getString("stu_password"));
+                student.setStuSex(rs.getString("stu_sex"));
+                students_Array.add(student);
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            mysqlConnection.closeResuletSet(rs);
+        }
+        return students_Array;
+    }
 
 }
+
+
+
+
+
+
+
+
