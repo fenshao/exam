37ac303f4e27e3ba81e920a596e8c1579e89d49e
wxpeppa <1218639030@qq.com>
2019-05-14 14:16:17 +0800

    默认

--- src/cn/ynni/exam/controllar/uPasswordServlet.java
+++ src/cn/ynni/exam/controllar/uPasswordServlet.java
@@ -20,27 +20,31 @@ public class uPasswordServlet extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
 
+        //获取 新旧密码 以及用户账号
         HttpSession session = req.getSession(true);
         String oldPass = req.getParameter("password");
         String newPass = req.getParameter("password2");
         String getId = (String) session.getAttribute("username");
 
+        //判断密码是否正确
         StudentService studentService = new StudentService();
         Student student = studentService.loginSystem(getId, oldPass);
+
         PrintWriter pw = resp.getWriter();
         if (student.getStuId() != null){
+            //修改密码
             Boolean result = studentService.updatePassword(getId,newPass);
             if(!result){
                 pw.println("<script language='javascript'>alert('修改失败')</script>");
-                resp.sendRedirect("studentIndex.jsp");
+                pw.println("<script>window.history.go(-1)</script>");
             }else {
                 pw.println("<script language='javascript'>alert('修改成功')</script>");
-                resp.sendRedirect("studentIndex.jsp");
+                pw.println("<script>window.history.go(-1)</script>");
             }
 
         }else {
             pw.println("<script language='javascript'>alert('修改失败')</script>");
-            resp.sendRedirect("studentIndex.jsp");
+            pw.println("<script>window.history.go(-1)</script>");
         }
     }
 }
