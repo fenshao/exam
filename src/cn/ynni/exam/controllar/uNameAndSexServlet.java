72c67a1bd7e2f62771c829a838acba8d9d06edae
wxpeppa <1218639030@qq.com>
2019-05-14 15:01:34 +0800

    默认

---
+++ src/cn/ynni/exam/controllar/uNameAndSexServlet.java
@@ -0,0 +1,37 @@
+package cn.ynni.exam.controllar;
+import cn.ynni.exam.service.StudentService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class uNameAndSexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+    protected void doPost(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        HttpSession session = req.getSession(true);
+        String getId = (String) session.getAttribute("username");
+        String getName = req.getParameter("name");
+        String getSex = req.getParameter("sex");
+
+        StudentService studentService = new StudentService();
+        PrintWriter pw = resp.getWriter();
+
+        Boolean result = studentService.updateNameAndSex(getId,getName,getSex);
+        if(!result){
+            pw.println("<script language='javascript'>alert('修改失败')</script>");
+            pw.println("<script>window.history.go(-1)</script>");
+        }else {
+            pw.println("<script language='javascript'>alert('修改成功')</script>");
+            pw.println("<script>window.history.go(-1)</script>");
+        }
+    }
+}
